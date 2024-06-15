1f1eff25e9077458e63afe59220d897cdfcda3ef
Amba <67413568+amba666@users.noreply.github.com>
2024-06-15 15:18:48 +0300

    adding crispy forms

--- website/settings.py
+++ website/settings.py
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS ="bootstrap5"
+
+
+CRISPY_TEMPLATE_PACK="bootstrap5"
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -124,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
+
+LOGout_REDIRECT_URL = '/login'
